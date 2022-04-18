fc6076ed2a78b8f2b130660933bf60cae12b13d3
gtpepi <gtpepi@hotmail.com>
2022-04-18 11:12:35 -0300

    Add xml and csv examples

--- demo2_asset_management/src/index.js
+++ demo2_asset_management/src/index.js
@@ -1,17 +1,22 @@
 import _ from 'lodash';
 import './style.css';
 import Icon from './image.svg'; 
+import Data from './data.xml';
+import Notes from './data.csv';
 
 function component() {
   const element = document.createElement('div');
 
   // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hellooooooooooooooo', 'webpack'], ' ');
+  element.innerHTML = _.join(['Test', 'webpack'], ' ');
   element.classList.add('hello');
 
   const myIcon = new Image();
   myIcon.src = Icon;
 
+  console.log(Data);
+  console.log(Notes);
+  
   return element;
 }
 
