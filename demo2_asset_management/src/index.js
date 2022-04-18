a4d35b8c081096e831956c265484e9bb826c168d
a4 <gtpepi@hotmail.com>
2022-04-18 10:31:58 -0300

    Update demo]

--- demo2_asset_management/src/index.js
+++ demo2_asset_management/src/index.js
@@ -1,14 +1,19 @@
 import _ from 'lodash';
-import './style.css'; 
+import './style.css';
+import Icon from './image.svg'; 
 
 function component() {
   const element = document.createElement('div');
 
   // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.innerHTML = _.join(['Hellooooooooooooooo', 'webpack'], ' ');
   element.classList.add('hello');
+
+  const myIcon = new Image();
+  myIcon.src = Icon;
 
   return element;
 }
 
 document.body.appendChild(component());
+
