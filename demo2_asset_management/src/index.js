267c2f6d3470b581b52d0a365122c2832650cda9
gtpepi <gtpepi@hotmail.com>
2022-04-18 18:10:18 -0300

    Add live reload exoad through devtools example

--- demo2_asset_management/src/index.js
+++ demo2_asset_management/src/index.js
@@ -1,33 +1,17 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './image.svg'; 
-import Data from './data.xml';
-import Notes from './data.csv';
-import toml from './data.toml';
-import yaml from './data.yaml';
-import json from './data.json5';
-
-console.log(toml.title); // output `TOML Example`
-console.log(toml.owner.name); // output `Tom Preston-Werner`
-
-console.log(yaml.title); // output `YAML Example`
-console.log(yaml.owner.name); // output `Tom Preston-Werner`
-
-console.log(json.title); // output `JSON5 Example`
-console.log(json.owner.name); // output `Tom Preston-Werner`
+import printMe from './print.js';
 
 function component() {
   const element = document.createElement('div');
 
   // Lodash, now imported by this script
   element.innerHTML = _.join(['Test', 'webpack'], ' ');
-  element.classList.add('hello');
+  const btn = document.createElement('button');
+  
+  btn.innerHTML = 'Click me and check the console!!!';
+  btn.onclick = printMe;
 
-  const myIcon = new Image();
-  myIcon.src = Icon;
-
-  console.log(Data);
-  console.log(Notes);
+  element.appendChild(btn);
 
   return element;
 }
--- demo2_asset_management/webpack.config.js
+++ demo2_asset_management/webpack.config.js
@@ -1,25 +1,25 @@
 const path = require('path');
+const HtmlWebpackPLugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js', 
-    path: path.resolve(__dirname, 'dist'),
+  mode: 'development',
+  entry: {
+    index: './src/index.js',
+    print: './src/print.js',
   },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'], 
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource',
-      },
-    ],
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPLugin({
+      title: 'Development',
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    publicPath: "/",
   },
 };
